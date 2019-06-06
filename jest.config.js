2728b1c2e69dbda2169f5bbc2cbd7804d7eb05f0
lnked <ed.proff@gmail.com>
2019-06-06 09:36:19 +0300

    initial

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue',
+    'ts',
+    'tsx'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    '/Users/lnked/web/starters/vue/vue-vuex-typescript-starter/node_modules/jest-watch-typeahead/filename.js',
+    '/Users/lnked/web/starters/vue/vue-vuex-typescript-starter/node_modules/jest-watch-typeahead/testname.js'
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  }
+}
